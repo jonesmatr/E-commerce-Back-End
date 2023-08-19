e964e6d854e92248fb3cecfa6afc2bbd8eb4aeae
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-08-19 16:01:56 -0400

    Made correction to the .belongsToMany in the index.js file for both Tag and Product.

--- models/index.js
+++ models/index.js
@@ -9,20 +9,26 @@ Product.belongsTo(Category, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE',
 });
+
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
+  as: 'products',  // Setting the alias for Product -> Tag
   foreignKey: 'product_id',
 });
+
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
+  as: 'tags',  // Setting the alias for Tag -> Product
   foreignKey: 'tag_id',
 });
+
 module.exports = {
   Product,
   Category,
